52380b3f78406cc864658be23b8808e8a5c87c26
Ahmed Ibrahim <ahmedibrahimhassan654@gmail.com>
2020-04-25 14:49:52 +0200

    modifiy online payment

--- membership.js
+++ membership.js
@@ -1,40 +1,55 @@
-class Paynent{
+class Payment{
     constructor(productPrice,discount,userMembership){
         this.productPrice=productPrice,
         this.discount=discount,
         this.userMembership=userMembership
-        this.onlinePayment=0
-       
-
+        
+        
     }
 
-
+    
     calculation(){
-       
+        
+      
         let productPrice=this.productPrice
         let discount=(productPrice/100)*this.discount
         let userMembership=this.userMembership
+        
         let elbaqeBadElkasm=productPrice-discount
         let qemaelkasm=discount
         let luvinamony=qemaelkasm*(50/100)
-
+        this.onlinePayment=function(){
+            if(this.discount>=10){
+                return 3.5
+            }else{
+                return 0
+            }
+        }
         this.elkasm=function(){
+           
             if(this.discount>=15 && this.userMembership==="silver"){
-             this.onlinePayment=3.5
+                
+             
                 return 2/5
+                
               
             }else if (this.discount>=10 && this.userMembership==="silver"){
-                this.onlinePayment=3.5
+                
                 return 1/4
+                
             }else if (this.discount>=5 && this.userMembership==="silver"){
                 return 0
             }
             else if (this.discount<5 && this.userMembership==="silver"){
-                this.onlinePayment=0
-                return 0
+               
+                 return 0
             }
+           
         }
-        let elbake=qemaelkasm-(luvinamony+this.onlinePayment)
+        
+        
+        
+        let elbake=qemaelkasm-(luvinamony+this.onlinePayment())
         let shownPrice=productPrice-(elbake*this.elkasm())
 
 
@@ -59,7 +74,7 @@ class Paynent{
     } 
 }
 
- const pay1=new Paynent(200,4,'silver')
+ const pay1=new Payment(200,25,'silver')
 
  console.log(pay1.calculation());
  
